14feded28dbec5ffa8f82d612aefa539ede14b16
tekuto <g.tekuto@gmail.com>
2013-11-08 04:12:56 +0900

    プロジェクト作成 以下の型を追加 ・gfBool ・gfSize ・gfByte ・gfByte ・gfShort ・gfShort ・gfInt ・gfInt ・gfLong ・gfLong ・gfFloat ・gfDouble ・gfStringChar ・gfUtf8Char ・gfUtf16Char ・gfUtf32Char

---
+++ inc/gf/common/primitives.h
@@ -0,0 +1,27 @@
+﻿#ifndef GF_COMMON_PRIMITIVES_H
+#define GF_COMMON_PRIMITIVES_H
+
+#include <uchar.h>
+
+typedef bool gfBool;
+
+typedef size_t gfSize;
+
+typedef char gfByte;
+typedef unsigned char gfUByte;
+typedef short gfShort;
+typedef unsigned short gfUShort;
+typedef int gfInt;
+typedef unsigned int gfUInt;
+typedef long long gfLong;
+typedef unsigned long long gfULong;
+
+typedef float gfFloat;
+typedef double gfDouble;
+
+typedef char gfStringChar;
+typedef char gfUtf8Char;
+typedef char16_t gfUtf16Char;
+typedef char32_t gfUtf32Char;
+
+#endif  // GF_COMMON_PRIMITIVES_H
