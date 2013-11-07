1bb04876c74a58bf6d0eea7bf6b088ccdd48fba4
tekuto <g.tekuto@gmail.com>
2013-11-08 04:27:57 +0900

    以下のマクロを追加 ・GFEXPORT ・GFCONSTRUCTOR(linux用) ・GFDESTRUCTOR(linux用)

---
+++ inc/gf/util/export.h
@@ -0,0 +1,12 @@
+﻿#ifndef GF_UTIL_EXPORT_H
+#define GF_UTIL_EXPORT_H
+
+#if defined OS_LINUX    // OS
+#   define GFEXPORT __attribute__( ( visibility( "default" ) ) )
+#elif defined OS_WINDOWS    // OS
+#   define GFEXPORT __declspec( dllexport )
+#else   // OS
+#   error 未対応のOS
+#endif  // OS
+
+#endif  // GF_UTIL_EXPORT_H
---
+++ inc/gf/util/import.h
@@ -0,0 +1,14 @@
+﻿#ifndef GF_UTIL_IMPORT_H
+#define GF_UTIL_IMPORT_H
+
+#ifndef GFEXPORT
+#   if defined OS_LINUX // OS
+#       define GFEXPORT
+#   elif defined OS_WINDOWS // OS
+#       define GFEXPORT __declspec( dllimport )
+#   else   // OS
+#       error 未対応のOS
+#   endif  // OS
+#endif  // GFEXPORT
+
+#endif  // GF_UTIL_IMPORT_H
