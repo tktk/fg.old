f36a4e61f3bd0043934cf19e8cff4c646eddc063
tekuto <g.tekuto@gmail.com>
2013-11-10 07:32:06 +0900

    以下のマクロを追加 ・GFSTRUCT ・GFKEYSTRUCT 以下の構造体の宣言を追加 ・gfGameConfig 以下の関数の宣言を追加 ・gfGameConfigNew()

---
+++ inc/gf/main/gameconfig.h
@@ -0,0 +1,17 @@
+﻿#ifndef GF_MAIN_GAMECONFIG_H
+#define GF_MAIN_GAMECONFIG_H
+
+#include "gf/util/import.h"
+#include "gf/util/externc.h"
+#include "gf/util/struct.h"
+
+GFSTRUCT( gf, GameConfig )
+
+GFEXTERNC_START
+
+GFEXPORT gfGameConfig * gfGameConfigNew(
+);
+
+GFEXTERNC_END
+
+#endif  // GF_MAIN_GAMECONFIG_H
