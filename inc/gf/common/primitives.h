514cc9d5ff869722e16b4a926484bb376d2eb552
tekuto <g.tekuto@gmail.com>
2013-11-14 17:07:47 +0900

    以下の列挙体の値を削除 ・GF_UTF8_MAX_CHAR_LENGTH ・GF_UTF16_MAX_CHAR_LENGTH ・GF_UTF32_MAX_CHAR_LENGTH 以下の構造体の宣言を追加 ・GfString ・GfUtf8 ・GfUtf16 ・GfUtf32 以下の関数の宣言を追加 ・gfStringFromUtf8CharPtr() ・gfStringFromUtf16CharPtr() ・gfStringFromUtf32CharPtr() ・gfStringNew() ・gfStringNewFromUtf8() ・gfStringNewFromUtf8CharPtr() ・gfStringNewFromUtf16() ・gfStringNewFromUtf16CharPtr() ・gfStringNewFromUtf32() ・gfStringNewFromUtf32CharPtr() ・gfStringFree() ・gfUtf8FromStringCharPtr() ・gfUtf8FromUtf16CharPtr() ・gfUtf8FromUtf32CharPtr() ・gfUtf8New() ・gfUtf8NewFromString() ・gfUtf8NewFromStringCharPtr() ・gfUtf8NewFromUtf16() ・gfUtf8NewFromUtf16CharPtr() ・gfUtf8NewFromUtf32() ・gfUtf8NewFromUtf32CharPtr() ・gfUtf8Free() ・gfUtf16FromStringCharPtr() ・gfUtf16FromUtf8CharPtr() ・gfUtf16FromUtf32CharPtr() ・gfUtf16New() ・gfUtf16NewFromString() ・gfUtf16NewFromStringCharPtr() ・gfUtf16NewFromUtf8() ・gfUtf16NewFromUtf8CharPtr() ・gfUtf16NewFromUtf32() ・gfUtf16NewFromUtf32CharPtr() ・gfUtf16Free() ・gfUtf32FromStringCharPtr() ・gfUtf32FromUtf8CharPtr() ・gfUtf32FromUtf16CharPtr() ・gfUtf32New() ・gfUtf32NewFromString() ・gfUtf32NewFromStringCharPtr() ・gfUtf32NewFromUtf8() ・gfUtf32NewFromUtf8CharPtr() ・gfUtf32NewFromUtf16() ・gfUtf32NewFromUtf16CharPtr() ・gfUtf32Free()

--- inc/gf/common/primitives.h
+++ inc/gf/common/primitives.h
@@ -3,6 +3,10 @@
 
 #include <stddef.h>
 
+#ifndef __cplusplus
+#   include <uchar.h>
+#endif  // __cplusplus
+
 typedef bool GfBool;
 
 typedef size_t GfSize;
@@ -18,5 +22,10 @@ typedef unsigned long long GfULong;
 
 typedef float GfFloat;
 typedef double GfDouble;
+
+typedef char GfStringChar;
+typedef char GfUtf8Char;
+typedef char16_t GfUtf16Char;
+typedef char32_t GfUtf32Char;
 
 #endif  // GF_COMMON_PRIMITIVES_H
--- inc/gf/common/string.h
+++ inc/gf/common/string.h
@@ -5,107 +5,279 @@
 #include "gf/util/import.h"
 #include "gf/util/externc.h"
 
-#ifndef __cplusplus
-#   include <uchar.h>
-#endif  // __cplusplus
-
 GFEXTERNC_START
 
-typedef char GfStringChar;
-typedef char GfUtf8Char;
-typedef char16_t GfUtf16Char;
-typedef char32_t GfUtf32Char;
-
-enum {
-    GF_UTF8_MAX_CHAR_LENGTH = 6,
-    GF_UTF16_MAX_CHAR_LENGTH = 2,
-    GF_UTF32_MAX_CHAR_LENGTH = 1,
-
-    GF_STRING_MAX_CHAR_LENGTH = GF_UTF8_MAX_CHAR_LENGTH,
-};
+typedef struct GfString GfString;
+typedef struct GfUtf8 GfUtf8;
+typedef struct GfUtf16 GfUtf16;
+typedef struct GfUtf32 GfUtf32;
 
 GFEXPORT GfBool gfStringFromUtf8(
-    GfStringChar *
-    , GfSize *
+    GfString *
+    , const GfUtf8 *
+);
+
+GFEXPORT GfBool gfStringFromUtf8CharPtr(
+    GfString *
     , const GfUtf8Char *
     , GfSize
 );
 
 GFEXPORT GfBool gfStringFromUtf16(
-    GfStringChar *
-    , GfSize *
+    GfString *
+    , const GfUtf16 *
+);
+
+GFEXPORT GfBool gfStringFromUtf16CharPtr(
+    GfString *
     , const GfUtf16Char *
     , GfSize
 );
 
 GFEXPORT GfBool gfStringFromUtf32(
-    GfStringChar *
-    , GfSize *
+    GfString *
+    , const GfUtf32 *
+);
+
+GFEXPORT GfBool gfStringFromUtf32CharPtr(
+    GfString *
     , const GfUtf32Char *
     , GfSize
 );
 
+GFEXPORT GfString * gfStringNew(
+);
+
+GFEXPORT GfString * gfStringNewFromUtf8(
+    const GfUtf8 *
+);
+
+GFEXPORT GfString * gfStringNewFromUtf8CharPtr(
+    const GfUtf8Char *
+    , GfSize
+);
+
+GFEXPORT GfString * gfStringNewFromUtf16(
+    const GfUtf16 *
+);
+
+GFEXPORT GfString * gfStringNewFromUtf16CharPtr(
+    const GfUtf16Char *
+    , GfSize
+);
+
+GFEXPORT GfString * gfStringNewFromUtf32(
+    const GfUtf32 *
+);
+
+GFEXPORT GfString * gfStringNewFromUtf32CharPtr(
+    const GfUtf32Char *
+    , GfSize
+);
+
+GFEXPORT void gfStringFree(
+    GfString *
+);
+
 GFEXPORT GfBool gfUtf8FromString(
-    GfUtf8Char *
-    , GfSize *
+    GfUtf8 *
+    , const GfString *
+);
+
+GFEXPORT GfBool gfUtf8FromStringCharPtr(
+    GfUtf8 *
     , const GfStringChar *
     , GfSize
 );
 
 GFEXPORT GfBool gfUtf8FromUtf16(
-    GfUtf8Char *
-    , GfSize *
+    GfUtf8 *
+    , const GfUtf16 *
+);
+
+GFEXPORT GfBool gfUtf8FromUtf16CharPtr(
+    GfUtf8 *
     , const GfUtf16Char *
     , GfSize
 );
 
 GFEXPORT GfBool gfUtf8FromUtf32(
-    GfUtf8Char *
-    , GfSize *
+    GfUtf8 *
+    , const GfUtf32 *
+);
+
+GFEXPORT GfBool gfUtf8FromUtf32CharPtr(
+    GfUtf8 *
     , const GfUtf32Char *
     , GfSize
 );
 
+GFEXPORT GfUtf8 * gfUtf8New(
+);
+
+GFEXPORT GfUtf8 * gfUtf8NewFromString(
+    const GfString *
+);
+
+GFEXPORT GfUtf8 * gfUtf8NewFromStringCharPtr(
+    const GfStringChar *
+    , GfSize
+);
+
+GFEXPORT GfUtf8 * gfUtf8NewFromUtf16(
+    const GfUtf16 *
+);
+
+GFEXPORT GfUtf8 * gfUtf8NewFromUtf16CharPtr(
+    const GfUtf16Char *
+    , GfSize
+);
+
+GFEXPORT GfUtf8 * gfUtf8NewFromUtf32(
+    const GfUtf32 *
+);
+
+GFEXPORT GfUtf8 * gfUtf8NewFromUtf32CharPtr(
+    const GfUtf32Char *
+    , GfSize
+);
+
+GFEXPORT void gfUtf8Free(
+    GfUtf8 *
+);
+
 GFEXPORT GfBool gfUtf16FromString(
-    GfUtf16Char *
-    , GfSize *
+    GfUtf16 *
+    , const GfString *
+);
+
+GFEXPORT GfBool gfUtf16FromStringCharPtr(
+    GfUtf16 *
     , const GfStringChar *
     , GfSize
 );
 
 GFEXPORT GfBool gfUtf16FromUtf8(
-    GfUtf16Char *
-    , GfSize *
+    GfUtf16 *
+    , const GfUtf8 *
+);
+
+GFEXPORT GfBool gfUtf16FromUtf8CharPtr(
+    GfUtf16 *
     , const GfUtf8Char *
     , GfSize
 );
 
 GFEXPORT GfBool gfUtf16FromUtf32(
-    GfUtf16Char *
-    , GfSize *
+    GfUtf16 *
+    , const GfUtf32 *
+);
+
+GFEXPORT GfBool gfUtf16FromUtf32CharPtr(
+    GfUtf16 *
     , const GfUtf32Char *
     , GfSize
 );
 
+GFEXPORT GfUtf16 * gfUtf16New(
+);
+
+GFEXPORT GfUtf16 * gfUtf16NewFromString(
+    const GfString *
+);
+
+GFEXPORT GfUtf16 * gfUtf16NewFromStringCharPtr(
+    const GfStringChar *
+    , GfSize
+);
+
+GFEXPORT GfUtf16 * gfUtf16NewFromUtf8(
+    const GfUtf8 *
+);
+
+GFEXPORT GfUtf16 * gfUtf16NewFromUtf8CharPtr(
+    const GfUtf8Char *
+    , GfSize
+);
+
+GFEXPORT GfUtf16 * gfUtf16NewFromUtf32(
+    const GfUtf32 *
+);
+
+GFEXPORT GfUtf16 * gfUtf16NewFromUtf32CharPtr(
+    const GfUtf32Char *
+    , GfSize
+);
+
+GFEXPORT void gfUtf16Free(
+    GfUtf16 *
+);
+
 GFEXPORT GfBool gfUtf32FromString(
-    GfUtf32Char *
-    , GfSize *
+    GfUtf32 *
+    , const GfString *
+);
+
+GFEXPORT GfBool gfUtf32FromStringCharPtr(
+    GfUtf32 *
     , const GfStringChar *
     , GfSize
 );
 
 GFEXPORT GfBool gfUtf32FromUtf8(
-    GfUtf32Char *
-    , GfSize *
+    GfUtf32 *
+    , const GfUtf8 *
+);
+
+GFEXPORT GfBool gfUtf32FromUtf8CharPtr(
+    GfUtf32 *
     , const GfUtf8Char *
     , GfSize
 );
 
 GFEXPORT GfBool gfUtf32FromUtf16(
-    GfUtf32Char *
-    , GfSize *
+    GfUtf32 *
+    , const GfUtf16 *
+);
+
+GFEXPORT GfBool gfUtf32FromUtf16CharPtr(
+    GfUtf32 *
     , const GfUtf16Char *
     , GfSize
+);
+
+GFEXPORT GfUtf32 * gfUtf32New(
+);
+
+GFEXPORT GfUtf32 * gfUtf32NewFromString(
+    const GfString *
+);
+
+GFEXPORT GfUtf32 * gfUtf32NewFromStringCharPtr(
+    const GfStringChar *
+    , GfSize
+);
+
+GFEXPORT GfUtf32 * gfUtf32NewFromUtf8(
+    const GfUtf8 *
+);
+
+GFEXPORT GfUtf32 * gfUtf32NewFromUtf8CharPtr(
+    const GfUtf8Char *
+    , GfSize
+);
+
+GFEXPORT GfUtf32 * gfUtf32NewFromUtf16(
+    const GfUtf16 *
+);
+
+GFEXPORT GfUtf32 * gfUtf32NewFromUtf16CharPtr(
+    const GfUtf16Char *
+    , GfSize
+);
+
+GFEXPORT void gfUtf32Free(
+    GfUtf32 *
 );
 
 GFEXTERNC_END
