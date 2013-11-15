687d2c99d0e4dabaae3dbd9d679d4c6e320f6337
tekuto <g.tekuto@gmail.com>
2013-11-15 18:31:39 +0900

    以下の関数の宣言を追加 ・gfGameConfigSetMain() ・gfGameConfigSetMainPath()

--- inc/gf/boot/gameconfig.h
+++ inc/gf/boot/gameconfig.h
@@ -1,6 +1,8 @@
 ﻿#ifndef GF_BOOT_GAMECONFIG_H
 #define GF_BOOT_GAMECONFIG_H
 
+#include "gf/string/utf32.h"
+#include "gf/common/primitives.h"
 #include "gf/util/import.h"
 #include "gf/util/externc.h"
 
@@ -13,6 +15,16 @@ GFEXPORT GfGameConfig * gfGameConfigNew(
 
 GFEXPORT void gfGameConfigFree(
     GfGameConfig *
+);
+
+GFEXPORT GfBool gfGameConfigSetMain(
+    GfGameConfig *
+    , const GfUtf32 *
+);
+
+GFEXPORT GfBool gfGameConfigSetMainPath(
+    GfGameConfig *
+    , const GfUtf32 *
 );
 
 GFEXTERNC_END
