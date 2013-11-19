00b9f48f0a2f0070a80a33c53737039e55e9c14e
tekuto <g.tekuto@gmail.com>
2013-11-19 21:23:37 +0900

    以下の構造体の宣言を追加 ・GfWindow ・GfWindowEventHandlers 以下の型を追加 ・GfWindowCloseEventHandler ・GfWindowPositionEventHandler ・GfWindowSizeEventHandler ・GfWindowPaintEventHandler 以下の関数の宣言を追加 ・gfWindowNew() ・gfWindowFree() ・gfWindowClose() ・gfWindowSetTitle() ・gfWindowSetPosition() ・gfWindowSetSize() ・gfWindowRepaint() ・gfWindowRepaintAll() ・gfWindowEventHandlerSetCloseEventHandler() ・gfWindowEventHandlerSetPositionEventHandler() ・gfWindowEventHandlerSetSizeEventHandler() ・gfWindowEventHandlerSetPaintEventHandler()

---
+++ inc/gf/window/eventhandlers.h
@@ -0,0 +1,66 @@
+﻿#ifndef GF_WINDOW_EVENTHANDLERS_H
+#define GF_WINDOW_EVENTHANDLERS_H
+
+typedef struct GfWindowEventHandlers GfWindowEventHandlers;
+
+#include "gf/window/window.h"
+#include "gf/common/primitives.h"
+
+typedef void( * GfWindowCloseEventHandler )(
+    GfWindow *
+);
+
+typedef void( * GfWindowPositionEventHandler )(
+    GfWindow *
+    , GfInt
+    , GfInt
+);
+
+typedef void( * GfWindowSizeEventHandler )(
+    GfWindow *
+    , GfInt
+    , GfInt
+);
+
+typedef void( * GfWindowPaintEventHandler )(
+    GfWindow *
+    , GfInt
+    , GfInt
+    , GfInt
+    , GfInt
+);
+
+#include "gf/util/import.h"
+
+GFEXPORT GfWindowEventHandlers * gfWindowEventHandlersNew(
+);
+
+GFEXPORT GfWindowEventHandlers * gfWindowEventHandlersClone(
+    const GfWindowEventHandlers *
+);
+
+GFEXPORT void gfWindowEventHandlersFree(
+    GfWindowEventHandlers *
+);
+
+GFEXPORT void gfWindowEventHandlersSetCloseEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowCloseEventHandler
+);
+
+GFEXPORT void gfWindowEventHandlersSetPositionEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowPositionEventHandler
+);
+
+GFEXPORT void gfWindowEventHandlersSetSizeEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowSizeEventHandler
+);
+
+GFEXPORT void gfWindowEventHandlersSetPaintEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowPaintEventHandler
+);
+
+#endif  // GF_WINDOW_EVENTHANDLERS_H
---
+++ inc/gf/window/window.h
@@ -0,0 +1,55 @@
+﻿#ifndef GF_WINDOW_WINDOW_H
+#define GF_WINDOW_WINDOW_H
+
+typedef struct GfWindow GfWindow;
+
+#include "gf/window/eventhandlers.h"
+#include "gf/string/utf32.h"
+#include "gf/common/primitives.h"
+#include "gf/util/import.h"
+
+GFEXPORT GfWindow * gfWindowNew(
+    const GfWindowEventHandlers *
+    , const GfUtf32 *
+    , GfInt
+    , GfInt
+);
+
+GFEXPORT void gfWindowFree(
+    GfWindow *
+);
+
+GFEXPORT void gfWindowClose(
+    GfWindow *
+);
+
+GFEXPORT GfBool gfWindowSetTitle(
+    GfWindow *
+    , const GfUtf32 *
+);
+
+GFEXPORT void gfWindowSetPosition(
+    GfWindow *
+    , GfInt
+    , GfInt
+);
+
+GFEXPORT void gfWindowSetSize(
+    GfWindow *
+    , GfInt
+    , GfInt
+);
+
+GFEXPORT void gfWindowRepaint(
+    GfWindow *
+    , GfInt
+    , GfInt
+    , GfInt
+    , GfInt
+);
+
+GFEXPORT void gfWindowRepaintAll(
+    GfWindow *
+);
+
+#endif  // GF_WINDOW_WINDOW_H
