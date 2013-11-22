1aed2e3f3fe74c83864cfe9c296d48fd0cafc8f2
tekuto <g.tekuto@gmail.com>
2013-11-22 22:12:46 +0900

    gf-windowの生成ルールを追加 以下の関数を追加 ・gfWindowEventHandlersNew() ・gfWindowEventHandlersClone() ・gfWindowEventHandlersFree() ・gfWindowEventHandlersSetCloseEventHandler() ・gfWindowEventHandlersCallCloseEventHandler() ・gfWindowEventHandlersSetPositionEventHandler() ・gfWindowEventHandlersCallPositionEventHandler() ・gfWindowEventHandlersSetSizeEventHandler() ・gfWindowEventHandlersCallSizeEventHandler() ・gfWindowEventHandlersSetPaintEventHandler() ・gfWindowEventHandlersCallPaintEventHandler() ・gfWindowNew() ・gfWindowFree() ・gfWindowClose() ・gfWindowSetTitle() ・gfWindowSetPosition() ・gfWindowSetSize() ・gfWindowRepaint() ・gfWindowRepaintAll()

---
+++ src/gf/window/eventhandlers.cpp
@@ -0,0 +1,88 @@
+﻿#include "gf/util/export.h"
+#include "gf/window/eventhandlers.h"
+
+#include "gf/window/window.h"
+#include "gf/common/primitives.h"
+
+GfWindowEventHandlers * gfWindowEventHandlersNew(
+)
+{
+    return nullptr;
+}
+
+GfWindowEventHandlers * gfWindowEventHandlersClone(
+    const GfWindowEventHandlers *
+)
+{
+    return nullptr;
+}
+
+void gfWindowEventHandlersFree(
+    GfWindowEventHandlers *
+)
+{
+}
+
+void gfWindowEventHandlersSetCloseEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowCloseEventHandler
+)
+{
+}
+
+void gfWindowEventHandlersCallCloseEventHandler(
+    const GfWindowEventHandlers *
+    , GfWindow *
+)
+{
+}
+
+void gfWindowEventHandlersSetPositionEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowPositionEventHandler
+)
+{
+}
+
+void gfWindowEventHandlersCallPositionEventHandler(
+    const GfWindowEventHandlers *
+    , GfWindow *
+    , GfInt
+    , GfInt
+)
+{
+}
+
+void gfWindowEventHandlersSetSizeEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowSizeEventHandler
+)
+{
+}
+
+void gfWindowEventHandlersCallSizeEventHandler(
+    const GfWindowEventHandlers *
+    , GfWindow *
+    , GfInt
+    , GfInt
+)
+{
+}
+
+void gfWindowEventHandlersSetPaintEventHandler(
+    GfWindowEventHandlers *
+    , GfWindowPaintEventHandler
+)
+{
+}
+
+void gfWindowEventHandlersCallPaintEventHandler(
+    const GfWindowEventHandlers *
+    , GfWindow *
+    , GfInt
+    , GfInt
+    , GfInt
+    , GfInt
+)
+{
+}
---
+++ src/gf/window/window.cpp
@@ -0,0 +1,68 @@
+﻿#include "gf/util/export.h"
+#include "gf/window/window.h"
+
+#include "gf/window/eventhandlers.h"
+#include "gf/string/utf32.h"
+#include "gf/common/primitives.h"
+
+GfWindow * gfWindowNew(
+    const GfWindowEventHandlers *
+    , const GfUtf32 *
+    , GfInt
+    , GfInt
+)
+{
+    return nullptr;
+}
+
+void gfWindowFree(
+    GfWindow *
+)
+{
+}
+
+void gfWindowClose(
+    GfWindow *
+)
+{
+}
+
+GfBool gfWindowSetTitle(
+    GfWindow *
+    , const GfUtf32 *
+)
+{
+    return false;
+}
+
+void gfWindowSetPosition(
+    GfWindow *
+    , GfInt
+    , GfInt
+)
+{
+}
+
+void gfWindowSetSize(
+    GfWindow *
+    , GfInt
+    , GfInt
+)
+{
+}
+
+void gfWindowRepaint(
+    GfWindow *
+    , GfInt
+    , GfInt
+    , GfInt
+    , GfInt
+)
+{
+}
+
+void gfWindowRepaintAll(
+    GfWindow *
+)
+{
+}
