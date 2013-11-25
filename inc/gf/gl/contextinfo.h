69c857915de1cd613063cd894b6fa6d655bab9c1
tekuto <g.tekuto@gmail.com>
2013-11-25 21:32:13 +0900

    以下の構造体の宣言を追加 ・GfGLContextInfo 以下の関数の宣言を追加 ・gfGLContextInfoNew() ・gfGLContextInfoClone() ・gfGLContextInfoFree()

---
+++ inc/gf/gl/contextinfo.h
@@ -0,0 +1,21 @@
+﻿#ifndef GF_GL_CONTEXTINFO_H
+#define GF_GL_CONTEXTINFO_H
+
+typedef struct GfGLContextInfo GfGLContextInfo;
+
+#include "gf/util/import.h"
+
+GFEXPORT GfGLContextInfo * gfGLContextInfoNew(
+);
+
+GFEXPORT GfGLContextInfo * gfGLContextInfoClone(
+    const GfGLContextInfo *
+);
+
+GFEXPORT void gfGLContextInfoFree(
+    GfGLContextInfo *
+);
+
+//TODO
+
+#endif  // GF_GL_CONTEXTINFO_H
