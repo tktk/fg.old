5596f0712a3e4cc84ea8265ea4d6ca90b48bd6ba
tekuto <g.tekuto@gmail.com>
2013-11-25 22:47:11 +0900

    以下のマクロを削除 ・GFCONSTRUCTOR ・GFDESTRUCTOR

--- inc/gf/util/linux/export.h
+++
@@ -1,7 +0,0 @@
-﻿#ifndef GF_UTIL_LINUX_EXPORT_H
-#define GF_UTIL_LINUX_EXPORT_H
-
-#define GFCONSTRUCTOR extern "C" __attribute__( ( constructor ) )
-#define GFDESTRUCTOR extern "C" __attribute__( ( destructor ) )
-
-#endif  // GF_UTIL_LINUX_EXPORT_H
