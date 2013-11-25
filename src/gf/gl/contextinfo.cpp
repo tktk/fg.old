4ebeebc72a8b4115f58dcf397c07fcbce07a779b
tekuto <g.tekuto@gmail.com>
2013-11-25 21:37:30 +0900

    以下の関数を追加 ・gfGLContextInfoNew() ・gfGLContextInfoClone() ・gfGLContextInfoFree()

---
+++ src/gf/gl/contextinfo.cpp
@@ -0,0 +1,21 @@
+﻿#include "gf/util/export.h"
+#include "gf/gl/contextinfo.h"
+
+GfGLContextInfo * gfGLContextInfoNew(
+)
+{
+    return nullptr;
+}
+
+GfGLContextInfo * gfGLContextInfoClone(
+    const GfGLContextInfo *
+)
+{
+    return nullptr;
+}
+
+void gfGLContextInfoFree(
+    GfGLContextInfo *
+)
+{
+}
