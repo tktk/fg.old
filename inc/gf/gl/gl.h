6e9298fb1441784ceafffb19a1901c81b96c35b0
tekuto <g.tekuto@gmail.com>
2013-11-25 20:56:59 +0900

    以下の関数の宣言を追加 ・gfGLSwapBuffers()

---
+++ inc/gf/gl/gl.h
@@ -0,0 +1,15 @@
+﻿#ifndef GF_GL_GL_H
+#define GF_GL_GL_H
+
+#include "gf/window/window.h"
+#include "gf/util/import.h"
+
+GFEXPORT void gfGLSwapBuffers(
+    GfWindow *
+);
+
+#include "gf/gl/types.h"
+#include "gf/gl/constants.h"
+#include "gf/gl/functions.h"
+
+#endif  // GF_GL_GL_H
