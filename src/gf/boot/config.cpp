6d649bb7eb6a697dc3975e2e53afd7cbf10c24d9
tekuto <g.tekuto@gmail.com>
2013-12-02 16:36:51 +0900

    以下の関数を追加 ・gfBootConfigExistsGLDoubleBuffer() ・gfBootConfigGetGLDoubleBuffer() ・gfBootConfigSetGLDoubleBuffer() ・gfBootConfigClearGLDoubleBuffer() ・gfBootConfigExistsGLAuxBuffers() ・gfBootConfigGetGLAuxBuffers() ・gfBootConfigSetGLAuxBuffers() ・gfBootConfigClearGLAuxBuffers() ・gfBootConfigExistsGLRedBits() ・gfBootConfigGetGLRedBits() ・gfBootConfigSetGLRedBits() ・gfBootConfigClearGLRedBits() ・gfBootConfigExistsGLGreenBits() ・gfBootConfigGetGLGreenBits() ・gfBootConfigSetGLGreenBits() ・gfBootConfigClearGLGreenBits() ・gfBootConfigExistsGLBlueBits() ・gfBootConfigGetGLBlueBits() ・gfBootConfigSetGLBlueBits() ・gfBootConfigClearGLBlueBits() ・gfBootConfigExistsGLAlphaBits() ・gfBootConfigGetGLAlphaBits() ・gfBootConfigSetGLAlphaBits() ・gfBootConfigClearGLAlphaBits() ・gfBootConfigExistsGLDepthBits() ・gfBootConfigGetGLDepthBits() ・gfBootConfigSetGLDepthBits() ・gfBootConfigClearGLDepthBits() ・gfBootConfigExistsGLStencilBits() ・gfBootConfigGetGLStencilBits() ・gfBootConfigSetGLStencilBits() ・gfBootConfigClearGLStencilBits() ・gfBootConfigExistsGLAccumRedBits() ・gfBootConfigGetGLAccumRedBits() ・gfBootConfigSetGLAccumRedBits() ・gfBootConfigClearGLAccumRedBits() ・gfBootConfigExistsGLAccumGreenBits() ・gfBootConfigGetGLAccumGreenBits() ・gfBootConfigSetGLAccumGreenBits() ・gfBootConfigClearGLAccumGreenBits() ・gfBootConfigExistsGLAccumBlueBits() ・gfBootConfigGetGLAccumBlueBits() ・gfBootConfigSetGLAccumBlueBits() ・gfBootConfigClearGLAccumBlueBits() ・gfBootConfigExistsGLAccumAlphaBits() ・gfBootConfigGetGLAccumAlphaBits() ・gfBootConfigSetGLAccumAlphaBits() ・gfBootConfigClearGLAccumAlphaBits()

--- src/gf/boot/config.cpp
+++ src/gf/boot/config.cpp
@@ -30,3 +30,327 @@ FgBool gfBootConfigSetBinDir(
 {
     return false;
 }
+
+FgBool gfBootConfigExistsGLDoubleBuffer(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgBool gfBootConfigGetGLDoubleBuffer(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+void gfBoolConfigSetGLDoubleBuffer(
+    GfBootConfig *
+    , FgBool
+)
+{
+}
+
+void gfBootConfigClearGLDoubleBuffer(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLAuxBuffers(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLAuxBuffers(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLAuxBuffers(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLAuxBuffers(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLRedBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLRedBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLRedBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLRedBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLGreenBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLGreenBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLGreenBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLGreenBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLBlueBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLBlueBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLBlueBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLBlueBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLAlphaBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLAlphaBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLAlphaBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLAlphaBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLDepthBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLDepthBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLDepthBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLDepthBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLStencilBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLStencilBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLStencilBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLStencilBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLAccumRedBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLAccumRedBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLAccumRedBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLAccumRedBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLAccumGreenBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLAccumGreenBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLAccumGreenBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLAccumGreenBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLAccumBlueBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLAccumBlueBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLAccumBlueBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLAccumBlueBits(
+    GfBootConfig *
+)
+{
+}
+
+FgBool gfBootConfigExistsGLAccumAlphaBits(
+    const GfBootConfig *
+)
+{
+    return false;
+}
+
+FgInt gfBootConfigGetGLAccumAlphaBits(
+    const GfBootConfig *
+)
+{
+    return 0;
+}
+
+void gfBoolConfigSetGLAccumAlphaBits(
+    GfBootConfig *
+    , FgInt
+)
+{
+}
+
+void gfBootConfigClearGLAccumAlphaBits(
+    GfBootConfig *
+)
+{
+}
