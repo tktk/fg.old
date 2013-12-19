73f55dba0cc086b4d69cbeb540a30cb5e067e808
tekuto <g.tekuto@gmail.com>
2013-12-19 19:37:23 +0900

    以下の関数を追加 ・fgWindowCloseEventNew() ・fgWindowCloseEventClone() ・fgWindowCloseEventFree() ・fgWindowCloseEventGetSource()

---
+++ src/fg/window/closeevent.cpp
@@ -0,0 +1,31 @@
+﻿#include "fg/util/export.h"
+#include "fg/window/closeevent.h"
+
+#include "fg/window/window.h"
+
+FgWindowCloseEvent * fgWindowCloseEventNew(
+    FgWindow *
+)
+{
+    return nullptr;
+}
+
+FgWindowCloseEvent * fgWindowCloseEventClone(
+    const FgWindowCloseEvent *
+)
+{
+    return nullptr;
+}
+
+void fgWindowCloseEventFree(
+    FgWindowCloseEvent *
+)
+{
+}
+
+FgWindow * fgWindowCloseEventGetSource(
+    FgWindowCloseEvent *
+)
+{
+    return nullptr;
+}
