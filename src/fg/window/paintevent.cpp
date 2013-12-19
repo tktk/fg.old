c13e9cc5f147db4f68d82960c05e48e4f9e74e06
tekuto <g.tekuto@gmail.com>
2013-12-20 07:11:54 +0900

    以下の関数を追加 ・fgWindowPaintEventNew() ・fgWindowPaintEventClone() ・fgWindowPaintEventFree() ・fgWindowPaintEventGetSource() ・fgWindowPaintEventGetX() ・fgWindowPaintEventGetY() ・fgWindowPaintEventGetWidth() ・fgWindowPaintEventGetHeight()

---
+++ src/fg/window/paintevent.cpp
@@ -0,0 +1,60 @@
+﻿#include "fg/util/export.h"
+#include "fg/window/paintevent.h"
+
+#include "fg/window/window.h"
+#include "fg/common/primitives.h"
+
+FgWindowPaintEvent * fgWindowPaintEventNew(
+    FgWindow *
+)
+{
+    return nullptr;
+}
+
+FgWindowPaintEvent * fgWindowPaintEventClone(
+    const FgWindowPaintEvent *
+)
+{
+    return nullptr;
+}
+
+void fgWindowPaintEventFree(
+    FgWindowPaintEvent *
+)
+{
+}
+
+FgWindow * fgWindowPaintEventGetSource(
+    const FgWindowPaintEvent *
+)
+{
+    return nullptr;
+}
+
+FgInt fgWindowPaintEventGetX(
+    const FgWindowPaintEvent *
+)
+{
+    return 0;
+}
+
+FgInt fgWindowPaintEventGetY(
+    const FgWindowPaintEvent *
+)
+{
+    return 0;
+}
+
+FgInt fgWindowPaintEventGetWidth(
+    const FgWindowPaintEvent *
+)
+{
+    return 0;
+}
+
+FgInt fgWindowPaintEventGetHeight(
+    const FgWindowPaintEvent *
+)
+{
+    return 0;
+}
