98e82cca6843c68012f472e96982fb9daec4c07a
tekuto <g.tekuto@gmail.com>
2013-12-20 06:12:23 +0900

    以下の関数を追加 ・fgWindowSizeEventNew() ・fgWindowSizeEventClone() ・fgWindowSizeEventFree() ・fgWindowSizeEventGetSource() ・fgWindowSizeEventGetWidth() ・fgWindowSizeEventGetHeight()

---
+++ src/fg/window/sizeevent.cpp
@@ -0,0 +1,46 @@
+﻿#include "fg/util/export.h"
+#include "fg/window/sizeevent.h"
+
+#include "fg/window/window.h"
+#include "fg/common/primitives.h"
+
+FgWindowSizeEvent * fgWindowSizeEventNew(
+    FgWindow *
+)
+{
+    return nullptr;
+}
+
+FgWindowSizeEvent * fgWindowSizeEventClone(
+    const FgWindowSizeEvent *
+)
+{
+    return nullptr;
+}
+
+void fgWindowSizeEventFree(
+    FgWindowSizeEvent *
+)
+{
+}
+
+FgWindow * fgWindowSizeEventGetSource(
+    const FgWindowSizeEvent *
+)
+{
+    return nullptr;
+}
+
+FgInt fgWindowSizeEventGetWidth(
+    const FgWindowSizeEvent *
+)
+{
+    return 0;
+}
+
+FgInt fgWindowSizeEventGetHeight(
+    const FgWindowSizeEvent *
+)
+{
+    return 0;
+}
