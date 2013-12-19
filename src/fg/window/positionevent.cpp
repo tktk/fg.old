88228e3a45d5a05534bdd805c0d45560db830341
tekuto <g.tekuto@gmail.com>
2013-12-20 05:47:48 +0900

    以下の関数を追加 ・fgWindowPositionEventNew() ・fgWindowPositionEventClone() ・fgWindowPositionEventFree() ・fgWindowPositionEventGetSource() ・fgWindowPositionEventGetX() ・fgWindowPositionEventGetY()

---
+++ src/fg/window/positionevent.cpp
@@ -0,0 +1,46 @@
+﻿#include "fg/util/export.h"
+#include "fg/window/positionevent.h"
+
+#include "fg/window/window.h"
+#include "fg/common/primitives.h"
+
+FgWindowPositionEvent * fgWindowPositionEventNew(
+    FgWindow *
+)
+{
+    return nullptr;
+}
+
+FgWindowPositionEvent * fgWindowPositionEventClone(
+    const FgWindowPositionEvent *
+)
+{
+    return nullptr;
+}
+
+void fgWindowPositionEventFree(
+    FgWindowPositionEvent *
+)
+{
+}
+
+FgWindow * fgWindowPositionEventGetSource(
+    const FgWindowPositionEvent *
+)
+{
+    return nullptr;
+}
+
+FgInt fgWindowPositionEventGetX(
+    const FgWindowPositionEvent *
+)
+{
+    return 0;
+}
+
+FgInt fgWindowPositionEventGetY(
+    const FgWindowPositionEvent *
+)
+{
+    return 0;
+}
