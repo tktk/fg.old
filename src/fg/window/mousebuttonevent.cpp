de60a2a50f93631d55dfbed8ad2f2db63d39dfbb
tekuto <g.tekuto@gmail.com>
2013-12-21 07:31:36 +0900

    以下の関数を追加 ・fgWindowMouseButtonEventNew() ・fgWindowMouseButtonEventClone() ・fgWindowMouseButtonEventFree() ・fgWindowMouseButtonEventGetSource() ・fgWindowMouseButtonEventGetIndex() ・fgWindowMouseButtonEventGetPressed() ・fgWindowMouseButtonEventGetX() ・fgWindowMouseButtonEventGetY()

---
+++ src/fg/window/mousebuttonevent.cpp
@@ -0,0 +1,64 @@
+﻿#include "fg/util/export.h"
+#include "fg/window/mousebuttonevent.h"
+
+#include "fg/window/window.h"
+#include "fg/common/primitives.h"
+
+FgWindowMouseButtonEvent * fgWindowMouseButtonEventNew(
+    FgWindow *
+    , FgULong
+    , FgBool
+    , FgInt
+    , FgInt
+)
+{
+    return nullptr;
+}
+
+FgWindowMouseButtonEvent * fgWindowMouseButtonEventClone(
+    const FgWindowMouseButtonEvent *
+)
+{
+    return nullptr;
+}
+
+void fgWindowMouseButtonEventFree(
+    FgWindowMouseButtonEvent *
+)
+{
+}
+
+FgWindow * fgWindowMouseButtonEventGetSource(
+    const FgWindowMouseButtonEvent *
+)
+{
+    return nullptr;
+}
+
+FgULong fgWindowMouseButtonEventGetIndex(
+    const FgWindowMouseButtonEvent *
+)
+{
+    return 0;
+}
+
+FgBool fgWindowMouseButtonEventGetPressed(
+    const FgWindowMouseButtonEvent *
+)
+{
+    return false;
+}
+
+FgInt fgWindowMouseButtonEventGetX(
+    const FgWindowMouseButtonEvent *
+)
+{
+    return 0;
+}
+
+FgInt fgWindowMouseButtonEventGetY(
+    const FgWindowMouseButtonEvent *
+)
+{
+    return 0;
+}
