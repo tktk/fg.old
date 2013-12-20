83680f96a9db8e6d90428400a1a21ae1df08dca7
tekuto <g.tekuto@gmail.com>
2013-12-21 06:38:24 +0900

    以下の関数を追加 ・fgWindowKeyEventNew() ・fgWindowKeyEventClone() ・fgWindowKeyEventFree() ・fgWindowKeyEventGetSource() ・fgWindowKeyEventGetKey() ・fgWindowKeyEventGetCharPtr() ・fgWindowKeyEventGetPressed()

---
+++ src/fg/window/keyevent.cpp
@@ -0,0 +1,57 @@
+﻿#include "fg/util/export.h"
+#include "fg/window/keyevent.h"
+
+#include "fg/window/window.h"
+#include "fg/window/key.h"
+#include "fg/common/primitives.h"
+
+FgWindowKeyEvent * fgWindowKeyEventNew(
+    FgWindow *
+    , FgKey
+    , const FgUtf32Char *
+    , FgBool
+)
+{
+    return nullptr;
+}
+
+FgWindowKeyEvent * fgWindowKeyEventClone(
+    const FgWindowKeyEvent *
+)
+{
+    return nullptr;
+}
+
+void fgWindowKeyEventFree(
+    FgWindowKeyEvent *
+)
+{
+}
+
+FgWindow * fgWindowKeyEventGetSource(
+    const FgWindowKeyEvent *
+)
+{
+    return nullptr;
+}
+
+FgKey fgWindowKeyEventGetKey(
+    const FgWindowKeyEvent *
+)
+{
+    return FG_KEY_INVALID;
+}
+
+const FgUtf32Char * fgWindowKeyEventGetCharPtr(
+    const FgWindowKeyEvent *
+)
+{
+    return nullptr;
+}
+
+FgBool fgWindowKeyEventGetPressed(
+    const FgWindowKeyEvent *
+)
+{
+    return false;
+}
