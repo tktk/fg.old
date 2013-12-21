7cab2d9f3d14fd58203f0e8a6390eac69ae0bf76
tekuto <g.tekuto@gmail.com>
2013-12-21 21:17:44 +0900

    以下の関数を追加 ・fgWindowMouseMotionEventNew() ・fgWindowMouseMotionEventClone() ・fgWindowMouseMotionEventFree() ・fgWindowMouseMotionEventGetSource() ・fgWindowMouseMotionEventGetX() ・fgWindowMouseMotionEventGetY()

---
+++ src/fg/window/mousemotionevent.cpp
@@ -0,0 +1,48 @@
+﻿#include "fg/util/export.h"
+#include "fg/window/mousemotionevent.h"
+
+#include "fg/window/window.h"
+#include "fg/common/primitives.h"
+
+FgWindowMouseMotionEvent * fgWindowMouseMotionEventNew(
+    FgWindow *
+    , FgInt
+    , FgInt
+)
+{
+    return nullptr;
+}
+
+FgWindowMouseMotionEvent * fgWindowMouseMotionEventClone(
+    const FgWindowMouseMotionEvent *
+)
+{
+    return nullptr;
+}
+
+void fgWindowMouseMotionEventFree(
+    FgWindowMouseMotionEvent *
+)
+{
+}
+
+FgWindow * fgWindowMouseMotionEventGetSource(
+    const FgWindowMouseMotionEvent *
+)
+{
+    return nullptr;
+}
+
+FgInt fgWindowMouseMotionEventGetX(
+    const FgWindowMouseMotionEvent *
+)
+{
+    return 0;
+}
+
+FgInt fgWindowMouseMotionEventGetY(
+    const FgWindowMouseMotionEvent *
+)
+{
+    return 0;
+}
